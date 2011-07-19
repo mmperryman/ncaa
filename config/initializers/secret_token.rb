6efce30451d3869bae89b09768be54014cae7fe1
Matthew Perryman <mmperryman@gmail.com>
2011-07-19 15:47:57 -0400

    New App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ncaa::Application.config.secret_token = '47132574c5189fce10df37abc681c5e5610aeb635467101c6743b7b8105fb597edb624cf4552d4453a381373db0ac356fea80ab74d7e5985f9ae3ac4d61e5a10'
