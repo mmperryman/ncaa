cc0e65f3d67de99765d0727008fc8d609f55ca92
Matt Perryman <mmperryman@gmail.com>
2011-08-15 22:48:22 -0400

    sendgrid setup

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Ncaa::Application.initialize!
 WIN_THRESHOLD = 40
+
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
+}
