96c2fd8244247f3a077f2b8c6cbc80ce3e9327d7
Matt Perryman <mmperryman@gmail.com>
2011-09-01 07:55:40 -0400

    disabling picks

---
+++ public/javascripts/picker_post.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.team_pick').live('mouseover', function() {
+	$(this).children('.win_info').show();
+  })
+  $('.team_pick').live('mouseout', function() {
+	$(this).children('.win_info').hide();
+  })
+})
+
