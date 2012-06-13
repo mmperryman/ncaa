0cebe07071ff36176f60e54e3d45355c48f43262
Matt Perryman <mmperryman@gmail.com>
2012-06-12 22:26:07 -0400

    updating payments yo

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,6 +43,10 @@ class User < ActiveRecord::Base
     picks.find_all_by_period_id(Period.find_by_year('2012')) || []
   end
   
+  def current_payment
+    payments.find_by_period_id(Period.find_by_year('2012'))
+  end
+  
   def self.find_by_login_or_email(login)
      find_by_login(login) || find_by_email(login)
   end
