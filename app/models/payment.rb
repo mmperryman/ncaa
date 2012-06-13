ef3b29e41c20b6e8258a9cac14d1191d0ca4ccd6
Matt Perryman <mmperryman@gmail.com>
2012-06-12 22:09:10 -0400

    yup yup

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,4 +5,8 @@ class Payment < ActiveRecord::Base
   def self.current_payments
     Payment.find_all_by_period_id(Period.find_by_year('2012'))
   end
+  
+  def self.current_active_payments
+    Payment.current_payments.find_all_by_paid(true)
+  end
 end
