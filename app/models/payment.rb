6850e6b42001646c34c8e4c89b3efab33559e5aa
Matt Perryman <mmperryman@gmail.com>
2012-06-12 22:07:36 -0400

    resetting payments

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,8 @@
 class Payment < ActiveRecord::Base
   belongs_to :user
   has_one :period
+  
+  def self.current_payments
+    Payment.find_all_by_period_id(Period.find_by_year('2012'))
+  end
 end
