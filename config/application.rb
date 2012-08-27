4466039b23e8fd292080bf44071866a742ff51b0
Matt Perryman <mmperryman@gmail.com>
2012-08-26 22:14:11 -0400

    News~

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-OPENING_DAY = Date.new(2012,8,30)
+OPENING_DAY = Time.new(2012,8,30,21)
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
